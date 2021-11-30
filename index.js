c3689c18fda3ee04a501d27a9cf856ecbd26aae8
Bock <charles.v.bock@intel.com>
2021-11-30 13:53:40 -0700

    initial

--- index.js
+++ index.js
@@ -3,32 +3,33 @@ const github = require('@actions/github');
 
 try {
 
-  const pointsLabel = core.getInput('pointslabel');
-  const labelsFilter = core.getInput('labels');
-  const state = 'open'
-  const token = core.getInput('GITHUB_TOKEN');
+    const pointsLabel = core.getInput('pointslabel');
+    const labelsFilter = core.getInput('labels');
 
-  const octokit = new github.GitHub(token);
+    const getContents = async (pointsLabel) => {
+        const state = 'open'
+        const token = core.getInput('GITHUB_TOKEN');
+        const octokit = new github.GitHub(token);
+        const repository = github.context.repo;
+        const list = await octokit.issues.listForRepo({
+            ...repository,
+            state,
+            pointsLabel
+        });
+        return list.data
+    }
+    result = getContents(pointsLabel)
+    console.log(result)
+    console.log(`pointsLabel: ${pointsLabel}!`);
+    const pointscount = (pointsLabel * 5)
+    console.log(`pointscount: ${pointscount}!`);
 
-  const repository = github.context.repo;
+    core.setOutput("pointscount", pointscount);
 
-  const list = await octokit.issues.listForRepo({
-    ...repository,
-    state,
-    pointsLabel
-  });
-
-  console.log(list.data)
-  console.log(`pointsLabel: ${pointsLabel}!`);
-  const pointscount = (pointsLabel*5)
-  console.log(`pointscount: ${pointscount}!`);
-
-  core.setOutput("pointscount", pointscount);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
