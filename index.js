d666c62469b686c1ee29d6e389373a4796ffade3
Bock <charles.v.bock@intel.com>
2021-12-02 12:27:23 -0700

    initial

--- index.js
+++ index.js
@@ -3,37 +3,24 @@ const github = require('@actions/github');
 const { Octokit } = require("@octokit/rest");
 
 try {
-
     const pointsLabel = core.getInput('pointslabel');
     const labelsFilter = core.getInput('labels');
     const token = core.getInput('repo-token');
-    let nameAndRepo = core.getInput('repo').split("/")
+    let ownerAndRepo = core.getInput('repo').split("/")
+    let ownerValue = ownerAndRepo[0]
+    let repoValue = ownerAndRepo[1]
 
     const octokit = new Octokit({
         auth: token,
       });
-    
-    console.log(nameAndRepo)
-
-    console.log(core.getInput('repo'))
-    console.log("Asdasd")
 
     let result = octokit.rest.issues.listForRepo({
-        owner: nameAndRepo[1],
-        repo: nameAndRepo[0],
+        owner: ownerValue,
+        repo: repoValue
       });
 
-
     console.log(result)
-    console.log(`pointsLabel: ${pointsLabel}!`);
-    const pointscount = (pointsLabel * 5)
-    console.log(`pointscount: ${pointscount}!`);
-
-    core.setOutput("pointscount", pointscount);
-
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    core.setOutput("pointscount", 99);
 
 } catch (error) {
     core.setFailed(error.message);
