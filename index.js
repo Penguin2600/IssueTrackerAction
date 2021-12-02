d52dfa1564c5fc5ccd20c23c5d5d511028d97f5c
Bock <charles.v.bock@intel.com>
2021-12-02 14:10:15 -0700

    initial

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ const github = require('@actions/github');
 const { Octokit } = require("@octokit/rest");
 const { createActionAuth } = require("@octokit/auth-action");
 
-try {
-    const pointsLabel = core.getInput('pointslabel');
-    const labelsFilter = core.getInput('labels');
-    const token = core.getInput('repo-token');
-    let ownerAndRepo = core.getInput('repo').split("/")
-    let ownerValue = ownerAndRepo[0]
-    let repoValue = ownerAndRepo[1]
+async function run() {
+    try {
+        const pointsLabel = core.getInput('pointslabel');
+        const labelsFilter = core.getInput('labels');
+        const token = core.getInput('repo-token');
+        let ownerAndRepo = core.getInput('repo').split("/")
+        let ownerValue = ownerAndRepo[0]
+        let repoValue = ownerAndRepo[1]
 
-    const octokit = new github.Github(token);
+        const octokit = github.getOctokit(token)
 
-    let result = octokit.issues.listForRepo({
-        owner: ownerValue,
-        repo: repoValue
-      });
+        let result = await octokit.rest.issues.listForRepo({
+            owner: ownerValue,
+            repo: repoValue
+        });
 
-    console.log(result)
-    core.setOutput("pointscount", 99);
+        console.log(result)
+        core.setOutput("pointscount", 99);
 
-} catch (error) {
-    core.setFailed(error.message);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
